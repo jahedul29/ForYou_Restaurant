b6d2a92bd181608e0eadb436a4e412f8d3494d67
jahedul29 <jahedulh1@gmail.com>
2020-01-31 07:15:54 +0600

    addCategoryController&IndexAction

---
+++ ForYou/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ForYou.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForYou.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET:Admin/Category/Index
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Categories.ToListAsync());
+        }
+    }
+}
--- ForYou/Data/Migrations/20200131005437_addCategoryToDatabase.Designer.cs
+++ ForYou/Data/Migrations/20200131005437_addCategoryToDatabase.Designer.cs
@@ -4,14 +4,16 @@ using ForYou.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ForYou.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200131005437_addCategoryToDatabase")]
+    partial class addCategoryToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
