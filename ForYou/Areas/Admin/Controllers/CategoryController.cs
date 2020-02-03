6036f37e94e01af90e0aaa0921ef7a77da70a46b
jahedul29 <jahedulh1@gmail.com>
2020-02-04 01:32:51 +0600

    CompletingCategoryEditFunctionality

--- ForYou/Areas/Admin/Controllers/CategoryController.cs
+++ ForYou/Areas/Admin/Controllers/CategoryController.cs
@@ -46,5 +46,40 @@ namespace ForYou.Areas.Admin.Controllers
 
             return View(category);
         }
+
+        //GET:Admin/Category/Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category =await _db.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        //POST:Admin/Category/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                var categoryFromdb = await _db.Categories.FirstOrDefaultAsync(m => m.CategoryId == category.CategoryId);
+                categoryFromdb.CategoryName = category.CategoryName;
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
     }
 }
