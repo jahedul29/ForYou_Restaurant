98171fec50f1db6e825f236a9a40cf0fcfea32ab
jahedul29 <jahedulh1@gmail.com>
2020-05-17 12:24:57 +0600

    UpdatingExtensionMethodsAndAddingDropdown

--- ForYou/Extension/IEnumerableExtension.cs
+++ ForYou/Extension/IEnumerableExtension.cs
@@ -1,23 +1,22 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ForYou.Models;
-using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ForYou.Extension
 {
     public static class IEnumerableExtension
     {
-        public static IEnumerable<SelectListItem> ToSelectListItems<T>(this IEnumerable<T> items, int selectedValue)
+        public static IEnumerable<SelectListItem> ToSelectListItem<T>(this IEnumerable<T> items, int selectedValue)
         {
             return from item in items
-                select new SelectListItem
-                {
-                    Text = item.GetPropertyValue("Name"),
-                    Value = item.GetPropertyValue("Id"),
-                    Selected = item.GetPropertyValue("Id").Equals(selectedValue.ToString())
-                };
+                   select new SelectListItem
+                   {
+                       Text = item.GetPropertyValue("CategoryName"),
+                       Value = item.GetPropertyValue("CategoryId"),
+                       Selected = item.GetPropertyValue("CategoryId").Equals(selectedValue.ToString())
+                   };
         }
     }
 }
--- ForYou/Extension/ReflectionExtension.cs
+++ ForYou/Extension/ReflectionExtension.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ForYou.Extension
 {
-    public static class ReflectionExtension
+    public static class ReflectionExtention
     {
         public static string GetPropertyValue<T>(this T item, string propertyName)
         {
